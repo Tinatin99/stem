62042490e995f531e1a640febf89541a9703522d
Tinatin99 <tinatindavitadze7@gmail.com>
2025-02-19 20:30:25 +0400

    script.js

---
+++ script.js
@@ -0,0 +1,67 @@
+// // 3.დავალების პირობა:
+//  შექმენით მაღაზიის ობიექტი (გლობალური scope-ში), რომელიც შეიცავს პროდუქტის მონაცემებს (სახელი, ფასი, რაოდენობა საწყობში).
+//  შექმენით ფუნქცია (function scope), რომელიც მიიღებს მომხმარებლის შეკვეთას და დააბრუნებს ჯამურ ღირებულებას.
+//  გამოიყენეთ ბლოკის scope if პირობაში, რათა გამოიანგარიშოთ ფასდაკლება შეკვეთაზე (მაგალითად, 3-ზე მეტი პროდუქტის შეძენისას 10% ფასდაკლება).
+// დაბეჭდეთ საბოლოო თანხა ეკრანზე.
+
+//  გლობალური scope - პროდუქტის მონაცემები
+// const product = {
+//     name: "მაისური",
+//     price: 20,
+//     stock: 10
+// };
+
+
+
+const books = {
+    nameofbook:["Data_Tutashkhia","red and black", "Me, grandmother, Iliko, Hilarion", "guest_host"],    
+    price:29,
+    stock:72,
+    
+        }
+        function nameofbook(komedi){
+           return "xelmisawvdomia"
+        }
+       
+let result = nameofbook(books)
+console.log(result);
+
+
+
+
+// const books = {
+//     nameofbook:["Data_Tutashkhia","red and black", "Me, grandmother, Iliko, Hilarion", "guest_host"],    
+//     price:29,
+//     stock:72,    
+//         }
+//         function nameofbook(komedi){{       
+//           if(books.stock > 72)
+//              return "მარაგში საკმარისი რაოდენობა არ არის!"
+//             }
+// return books.price * 36;
+//   }
+//   let result = nameofbook(books)
+//   console.log(result);
+
+
+
+
+//   const books = {
+//     nameofbook:["Data_Tutashkhia","red and black", "Me, grandmother, Iliko, Hilarion", "guest_host"],    
+//     price:29,
+//     stock:72,    
+//         }
+//         function nameofbook(komedi){{       
+//           if(books.stock > 72)
+//              return "მარაგში საკმარისი რაოდენობა არ არის!"
+//             }
+// return books.price * 97;
+//   }
+//   let result = nameofbook(books)
+//   console.log(result);
+
+
+
+
+// ვცადე და არ გამომივიდა სამწუხაროდ
+    
